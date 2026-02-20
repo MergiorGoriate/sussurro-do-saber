fdce47e3dea7237417dc6a878e0a3b229f0d408f
Mergior Goriate <geniospurificado@gmail.com>
2026-02-20 03:05:17 +0200

    Restore Tailwind CDN and add log

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n';
 
+console.log("Starting Sussurros do Saber...");
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
