bc8eef569567252f808ac3a5777aa1fe57e39708
Mergior Goriate <geniospurificado@gmail.com>
2026-02-22 04:30:55 +0200

    Fix: Explicitly set assets directory for correct production pathing

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig(({ mode }) => {
       }
     },
     build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      emptyOutDir: true,
       rollupOptions: {
         input: {
           main: path.resolve(__dirname, 'index.html'),
